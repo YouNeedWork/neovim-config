f87da50284eadb11a3277825cafe3cc62d3ba58a
YusongWang <admin@wangyusong.com>
2025-08-06 11:27:09 +0800

    add C+i to show signtures infomation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,10 @@ vim.keymap.set("n", "-", "<cmd>Oil --float<CR>")
 vim.keymap.set("n", "\\", "<cmd>vsplit<CR>")
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>")
 -- Map Ctrl+H to move to the left window
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-h>", "", { noremap = true, silent = true })
 -- Map Ctrl+L to move to the right window
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-I>", function()
-	--	local node = require("nvim-treesitter.ts_utils").get_node_at_cursor()
-	--	if node and node:type() == "identifier" then
+vim.api.nvim_set_keymap("n", "<C-l>", "", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-i>", function()
 	vim.lsp.buf.signature_help()
-	--	else
-	--		print("Not on a method name")
-	--	end
 end)
