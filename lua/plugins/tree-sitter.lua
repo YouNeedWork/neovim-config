bf73d1807de8e85c968b447a475909b28426067d
dev <dev@dev.io>
2025-09-05 09:42:40 +0800

    update

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -7,35 +7,23 @@ return {
 		install_dir = vim.fn.stdpath("data") .. "/site",
 	},
 	config = function()
-		local ensure_installed =
-			{
-				"c3",
-				"zig",
-				"php",
-				"rust",
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"c",
-				"go",
-				"lua",
-				"jsdoc",
-				"bash",
-				"css",
-				"kotlin",
-			}, vim.api.nvim_create_autocmd("User", {
-				pattern = "TSUpdate",
-				callback = function()
-					require("nvim-treesitter.parsers").dyn = {
-						install_info = {
-							url = "https://github.com/17robots/tree-sitter-dyn",
-							branch = "neovim",
-							queries = "queries",
-						},
-					}
-				end,
-			})
-		vim.filetype.add({ extension = { dyn = "dyn" } })
+		local ensure_installed = {
+			"c3",
+			"zig",
+			"php",
+			"rust",
+			"vimdoc",
+			"javascript",
+			"typescript",
+			"c",
+			"go",
+			"lua",
+			"jsdoc",
+			"bash",
+			"css",
+			"kotlin",
+		}
+
 		require("nvim-treesitter").install(ensure_installed)
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = {
@@ -53,6 +41,7 @@ return {
 				"bash",
 				"css",
 				"kotlin",
+				"dyn",
 			},
 			callback = function()
 				vim.treesitter.start()
